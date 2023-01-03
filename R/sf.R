e41a092c5a8484d8b58f9d029353e46a76dff5c4
Ben Tupper <btupper@bigelow.org>
2023-01-03 10:48:32 -0500

    add sf_column and write_geopackage

--- R/sf.R
+++ R/sf.R
@@ -118,3 +118,38 @@ match_points_to_polygons <- function(pts, poly, form = 'string'){
           to_string(pts, poly))
   
 }
+
+
+#' Write to a geopackage file by writing to a temp file first and then
+#' copying to the specified destination.
+#' 
+#' @seealso \href{https://github.com/r-spatial/sf/issues/628#issuecomment-381690663}{sf issue 628}
+#' @export
+#' @param obj object to write
+#' @param dsn character, the file to write to
+#' @param overwrite, logical, overwite existing file?
+#' @param ... other arguments passed to \code{\link[sf]{write_sf}}
+#' @return obj invisibly
+write_gpkg <- function(obj, dsn, 
+                       overwrite = FALSE,
+                       ...){
+  dsnpath <- dirname(dsn)
+  if (!dir.exists(dsnpath)) stop("destination path doesn't exist: ", dsnpath)
+  tmpfile <- tempfile(fileext = ".gpkg")
+  obj <- try(sf::write_sf(obj, dsn=tmpfile, driver = "GPKG"))
+  if (inherits(obj, "try-error")){
+    print(obj)
+  } else {
+    ok <- file.copy(tmpfile, dsn, overwrite = overwrite)
+  }
+  invisible(obj)
+}
+
+#' Retrieve the attribute that names to sf column
+#' 
+#' @export
+#' @param x sf object
+#' @return character, the name of the sf column
+sf_column <- function(x){
+  attr(x, "sf_column")
+}

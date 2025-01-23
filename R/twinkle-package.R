95ee7c4c7118fe87f0f644136498f84654282f9c
Ben Tupper <btupper@bigelow.org>
2025-01-23 12:44:33 -0500

    add combine_ready()

--- R/twinkle-package.R
+++ R/twinkle-package.R
@@ -1,9 +1,8 @@
 #' twinkle: Tools for working with [https://CRAN.R-project.org/package=stars] and [simple features](https://CRAN.R-project.org/package=sf).
 #'
 #' @description Tools for working with [https://CRAN.R-project.org/package=stars] and [simple features](https://CRAN.R-project.org/package=sf).
-#' @docType package
 #' @name twinkle
 #' @importFrom graphics par
 #' @importFrom rlang .data := !! set_names
 #' @importFrom stats complete.cases runif na.omit
-NULL
+"_PACKAGE"
